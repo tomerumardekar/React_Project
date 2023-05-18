a5ee9349001b898dd252656e4c256edf6b4a21dd
tomerumardekar <tomtom1361991@gmail.com>
2023-05-18 11:19:07 +0300

    delete folder sandbox

--- my-app/src/sandbox/context/GrandChild.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useData } from "./DataProvider";
-
-export default function GrandChild() {
-  let data = useData();
-  return <div>{JSON.stringify(data)}</div>;
-}
--- my-app/src/sandbox/props/StringPropParent.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import StringPropChild from "./StringPropChild";
-
-export default function StringPropParent() {
-  let string = "Text from the father";
-  return <StringPropChild data={string} />;
-}
