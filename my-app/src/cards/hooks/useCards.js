f0aea14777b60a7febf252663bd9adde1f06f1c7
tomerumardekar <tomtom1361991@gmail.com>
2023-06-08 12:57:21 +0300

    navigate to my cards

--- my-app/src/cards/hooks/useCards.js
+++ my-app/src/cards/hooks/useCards.js
@@ -150,7 +150,7 @@ export default function useCards() {
       const card = await createCard(cardFromClient);
       requestStatus(false, null, null, card);
       snack("success", "A new business card has been created");
-      navigate(ROUTES.CARDS);
+      navigate(ROUTES.MY_CARDS);
     } catch (error) {
       requestStatus(false, error, null);
     }
