dd23981be0716a759a5eb64608a86488232f27e8
tomerumardekar <tomtom1361991@gmail.com>
2023-06-12 23:36:41 +0300

    design the page

--- my-app/src/cards/pages/ProfilePage.jsx
+++ my-app/src/cards/pages/ProfilePage.jsx
@@ -6,37 +6,69 @@ export default function UserPage() {
   const { user } = useUser();
 
   return (
-    <Container sx={{ width: "80vw", marginTop: "4vh" }}>
-      <img src={user?.image?.url} alt={user?.image?.alt} />
-      <Typography variant="body1" color="initial">
-        Name:{user?.firstName} {user?.name?.middle} {user?.last}{" "}
-      </Typography>{" "}
-      <br></br>
-      <Typography variant="body1" color="initial">
-        Email: {user?.email}{" "}
-      </Typography>{" "}
-      <br></br>
-      <Typography variant="body1" color="initial">
-        Phone: {user?.phone}{" "}
-      </Typography>{" "}
-      <br></br>
-      <Typography variant="body1" color="initial">
-        Address:{user?.address?.city} {user?.address?.street}{" "}
-        {user?.address?.hoeNumber} {user?.address?.country} {user?.address?.zip}{" "}
-      </Typography>{" "}
-      <br></br>
-      <Typography variant="body1" color="initial">
-        IsBusiness: {user?.isBusiness ? "yes" : "no"}{" "}
-      </Typography>{" "}
-      <br></br>
-      <Typography variant="body1" color="initial">
-        IsAdmin: {user?.isAdmin ? "yes" : "no"}{" "}
-      </Typography>{" "}
-      <br></br>
-      <Typography variant="body1" color="initial">
-        User id: {user?.user_id}{" "}
-      </Typography>{" "}
-      <br></br>
+    <Container sx={{ width: "80vw", marginTop: "10vh" }}>
+      <Typography
+        variant="h3"
+        color="initial"
+        style={{ fontFamily: "Arial", marginBottom: "2vh" }}
+      >
+        Hello, {user?.firstName}!
+      </Typography>
+      <img
+        src={user?.image?.url}
+        alt={user?.image?.alt}
+        style={{ marginBottom: "2vh" }}
+      />
+      <Typography
+        variant="h5"
+        color="initial"
+        style={{ fontFamily: "Arial", marginBottom: "1vh" }}
+      >
+        Name: {user?.firstName} {user?.name?.middle} {user?.last}
+      </Typography>
+      <Typography
+        variant="h6"
+        color="initial"
+        style={{ fontFamily: "Arial", marginBottom: "1vh" }}
+      >
+        Email: {user?.email}
+      </Typography>
+      <Typography
+        variant="h6"
+        color="initial"
+        style={{ fontFamily: "Arial", marginBottom: "1vh" }}
+      >
+        Phone: {user?.phone}
+      </Typography>
+      <Typography
+        variant="h6"
+        color="initial"
+        style={{ fontFamily: "Arial", marginBottom: "1vh" }}
+      >
+        Address: {user?.address?.city} {user?.address?.street}{" "}
+        {user?.address?.hoeNumber} {user?.address?.country} {user?.address?.zip}
+      </Typography>
+      <Typography
+        variant="h6"
+        color="initial"
+        style={{ fontFamily: "Arial", marginBottom: "1vh" }}
+      >
+        IsBusiness: {user?.isBusiness ? "yes" : "no"}
+      </Typography>
+      <Typography
+        variant="h6"
+        color="initial"
+        style={{ fontFamily: "Arial", marginBottom: "1vh" }}
+      >
+        IsAdmin: {user?.isAdmin ? "yes" : "no"}
+      </Typography>
+      <Typography
+        variant="h6"
+        color="initial"
+        style={{ fontFamily: "Arial", marginBottom: "1vh" }}
+      >
+        User ID: {user?.user_id}
+      </Typography>
     </Container>
   );
 }
