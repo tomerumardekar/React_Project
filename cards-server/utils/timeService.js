606ac17a72e33580f7cce167abc39e24971ab68b
tomerumardekar <tomtom1361991@gmail.com>
2023-06-25 18:55:31 +0300

    loggers

--- cards-server/utils/timeService.js
+++ cards-server/utils/timeService.js
@@ -1,17 +1,16 @@
 const currentTime = () => {
-  const addLeadingZero = (value) => {
-    return value.toString().padStart(2, "0");
-  };
-
   let now = new Date();
-  let year = now.getFullYear();
-  let month = addLeadingZero(now.getMonth() + 1);
-  let day = addLeadingZero(now.getDate());
-  let hour = addLeadingZero(now.getHours());
-  let minute = addLeadingZero(now.getMinutes());
-  let second = addLeadingZero(now.getSeconds());
-
-  return { year, month, day, hour, minute, second };
+  let result = {};
+  result.year = now.getFullYear();
+  result.month = now.getMonth() + 1;
+  result.day = now.getDate();
+  result.hours = now.getHours();
+  result.minutes = now.getMinutes();
+  result.seconds = now.getSeconds();
+  for (const key in result) {
+    result[key] = result[key].toString().padStart(2, "0");
+  }
+  return result;
 };
 
-console.log(currentTime());
+module.exports = currentTime;
