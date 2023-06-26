4605441862bc7a5dbbd171c38c210c56d4d6e855
tomerumardekar <tomtom1361991@gmail.com>
2023-06-26 10:41:22 +0300

    const DB

--- cards-server/users/cards/models/cardsAccessDataService.js
+++ cards-server/users/cards/models/cardsAccessDataService.js
@@ -1,79 +1,87 @@
 const Card = require("./mongodb/card");
 
+const DB = "MONGODB";
+
 const createCard = async (normalizedCard) => {
-  try {
-    let card = new Card(normalizedCard);
-    await card.save();
-    console.log(card);
-  } catch (error) {
-    console.log(error);
-  }
+  if (DB == "MONGODB")
+    try {
+      let card = new Card(normalizedCard);
+      await card.save();
+      console.log(card);
+    } catch (error) {
+      console.log(error);
+    }
 };
 
 const getCards = async () => {
-  try {
-    const cards = await Card.find();
-    console.log(cards);
-  } catch (error) {
-    console.log(error);
-  }
+  if (DB == "MONGODB")
+    try {
+      const cards = await Card.find();
+      console.log(cards);
+    } catch (error) {
+      console.log(error);
+    }
 };
 
 const getCard = async (cardId) => {
-  try {
-    const card = await Card.findById(cardId);
-    console.log(card);
-  } catch (error) {
-    console.log(error);
-  }
+  if (DB == "MONGODB")
+    try {
+      const card = await Card.findById(cardId);
+      console.log(card);
+    } catch (error) {
+      console.log(error);
+    }
 };
 
 const getMyCards = async (userId) => {
-  try {
-    let cards = await Card.find({ user_id: userId });
-    console.log(cards);
-  } catch (error) {
-    console.log(error);
-  }
+  if (DB == "MONGODB")
+    try {
+      let cards = await Card.find({ user_id: userId });
+      console.log(cards);
+    } catch (error) {
+      console.log(error);
+    }
 };
 
 const updateCard = async (cardId, normalizedCard) => {
-  try {
-    let card = await Card.findByIdAndUpdate(cardId, normalizedCard, {
-      new: true,
-    });
-    if (!card) {
-      throw new Error("The card with this id didnt found");
+  if (DB == "MONGODB")
+    try {
+      let card = await Card.findByIdAndUpdate(cardId, normalizedCard, {
+        new: true,
+      });
+      if (!card) {
+        throw new Error("The card with this id didnt found");
+      }
+      console.log(card);
+    } catch (error) {
+      console.log(error);
     }
-    console.log(card);
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 const likeCard = async (cardId, userId) => {
-  try {
-    let card = await Card.findById(cardId);
-    if (!card)
-      throw new Error("A card with this ID cannot be found in the database");
+  if (DB == "MONGODB")
+    try {
+      let card = await Card.findById(cardId);
+      if (!card)
+        throw new Error("A card with this ID cannot be found in the database");
 
-    if (card.likes.includes(userId)) {
-      card = await Card.findByIdAndUpdate(
-        cardId,
-        { $pull: { likes: userId } },
-        { new: true }
-      );
-    } else {
-      card = await Card.findByIdAndUpdate(
-        cardId,
-        { $push: { likes: userId } },
-        { new: true }
-      );
+      if (card.likes.includes(userId)) {
+        card = await Card.findByIdAndUpdate(
+          cardId,
+          { $pull: { likes: userId } },
+          { new: true }
+        );
+      } else {
+        card = await Card.findByIdAndUpdate(
+          cardId,
+          { $push: { likes: userId } },
+          { new: true }
+        );
+      }
+      console.log(card);
+    } catch (error) {
+      console.log(error);
     }
-    console.log(card);
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 exports.createCard = createCard;
